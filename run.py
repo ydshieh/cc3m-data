f80a6a41797dfced38e09220794975cf6986bee5
ydshieh <ydshieh@user.noreply>
2021-07-31 19:06:24 +0200

    upload run.py

---
+++ run.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+
+# --------------------------------------------------------------------------------
+# Translate cc3m train dataset on GCP VMs with uploading to a Google Storage bucket
+
+hostname = subprocess.check_output('hostname', shell=True).decode('UTF-8')
+assert hostname.startswith('cc3m-data-')
+idx = int(hostname.replace('cc3m-data-', ''))
+idx = idx - 1
+
+start = idx * 100000
+end = (idx + 1) * 100000
+
+bucket_name = '[...]'
+blob_prefix = '[...]'
+upload_batch_size = 10000
+
+command = f'nohup python -u cc3m_processing.py --input_dir "./" --input_fn "cc3m_train.jsonl" --inf {start} --sup {end} --output_dir "./" --batch_size 100 --buf_size 100 --bucket_name {bucket_nam$
+print(command)
+os.system(command)
+
+# --------------------------------------------------------------------------------
+# Translate cc3m valid dataset locally without uploading
+
+start = 0
+end = 15840
+command = f'python cc3m_processing.py --input_dir "./" --input_fn "cc3m_valid.jsonl" --inf {start} --sup {end} --output_dir "./" --batch_size 100 --buf_size 100'
+print(command)
+os.system(command)
+
+# --------------------------------------------------------------------------------
