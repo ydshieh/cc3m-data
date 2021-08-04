395bded09ccec0749f1836019298563315769ceb
ydshieh <ydshieh@user.noreply>
2021-08-04 13:43:33 -0700

    improve image downloader

--- cc3m_image_downloader.py
+++ cc3m_image_downloader.py
@@ -12,6 +12,8 @@ from multiprocessing import Pool
 from tqdm import tqdm
 import logging
 import json
+import shutil
+from pathlib import Path
 
 # Setup
 logging.basicConfig(filename='cc3m_image_download.log', filemode='w', level=logging.INFO)
@@ -38,23 +40,37 @@ def process(entry, target_dir, fn_prefix):
 
         fn = f'{fn_prefix}_{image_id:08d}.jpg'  # create filename
         f_path = os.path.join(target_dir, fn)  # concat to get filepath
-
+        f_path_temp = Path(os.path.join('./tmp/', fn))
+       
         if not os.path.isfile(f_path):
 
-            req = requests.get(image_url, stream=True, timeout=1, verify=False).raw
-            image = Image.open(req).convert('RGB')
-            image = resize(image)  # resize PIL image
-            image.save(f_path)  # save PIL image
+            req = requests.get(image_url, stream=True, timeout=2, verify=False)
+            
+            # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+            req.raw.decode_content = True
+
+            # Open a local file with wb ( write binary ) permission.
+            with open(f_path_temp, 'wb') as fp:
+                shutil.copyfileobj(req.raw, fp)            
+            
+            with Image.open(f_path_temp).convert('RGB') as image:
+                img = resize(image)  # resize PIL image
+                img.save(f_path)  # save PIL image
+
+            f_path_temp.unlink(missing_ok=True)
 
     except Exception as e:
+    
+        f_path_temp.unlink(missing_ok=True)
         logging.info(" ".join(repr(e).splitlines()))
+        logging.info(f"error while downloading image_id: {entry['image_id']}")
         logging.error(image_url)
-
+        
 
 if __name__ == '__main__':
 
     n_processes = 2
-    buf_size_per_proc = 3
+    buf_size_per_proc = 500
     buf_size = buf_size_per_proc * n_processes
     buf = []
 
@@ -63,6 +79,8 @@ if __name__ == '__main__':
     target_dir = os.path.join(image_root_dir, fn_prefix)
     if not os.path.isdir(target_dir):
         os.makedirs(target_dir, exist_ok=True)
+    if not os.path.isdir('./tmp/'):
+        os.makedirs('./tmp/', exist_ok=True)
 
     cc3m_jsonl_file_path = './cc3m_valid.jsonl'
 
@@ -72,7 +90,7 @@ if __name__ == '__main__':
 
             entry = json.loads(line)
 
-            if entry['id'] >= 20:
+            if entry['id'] >= 1000:
                 break
 
             buf.append((entry, target_dir, fn_prefix))
@@ -81,7 +99,9 @@ if __name__ == '__main__':
 
                 with Pool(n_processes) as p:
                     r = p.starmap(process, buf)
+                    buf = [] 
 
         if len(buf) > 0:
             with Pool(n_processes) as p:
                 r = p.starmap(process, buf)
+                buf = []
