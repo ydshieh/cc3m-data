dac61c8e0d1ff990c4c08fa675192548c3d24d28
ydshieh <ydshieh@user.noreply>
2021-08-05 01:34:42 -0700

    update image downloader

--- cc3m_image_downloader.py
+++ cc3m_image_downloader.py
@@ -16,9 +16,13 @@ import shutil
 from pathlib import Path
 
 # Setup
-logging.basicConfig(filename='cc3m_image_download.log', filemode='w', level=logging.INFO)
-requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+tmp_dir = './tmp/'
+log_fn = 'cc3m_image_download.log'
+if not os.path.isdir(tmp_dir):
+    os.makedirs(tmp_dir, exist_ok=True)
 
+logging.basicConfig(filename=os.path.join(log_fn), filemode='w', level=logging.INFO)
+requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
 MAX_IMAGE_SIZE = 512
 
@@ -40,16 +44,19 @@ def process(entry, target_dir, fn_prefix):
 
         fn = f'{fn_prefix}_{image_id:08d}.jpg'  # create filename
         f_path = os.path.join(target_dir, fn)  # concat to get filepath
-        f_path_temp = Path(os.path.join('./tmp/', fn))
+        f_path_temp = Path(os.path.join(tmp_dir, fn))
        
         if not os.path.isfile(f_path):
 
             req = requests.get(image_url, stream=True, timeout=2, verify=False)
-            
+                        
             # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
             req.raw.decode_content = True
 
-            # Open a local file with wb ( write binary ) permission.
+            # check status
+            assert req.status_code == 200, "status != 200"
+
+            # Open a local file with wb (write binary) permission.
             with open(f_path_temp, 'wb') as fp:
                 shutil.copyfileobj(req.raw, fp)            
             
@@ -62,27 +69,32 @@ def process(entry, target_dir, fn_prefix):
     except Exception as e:
     
         f_path_temp.unlink(missing_ok=True)
-        logging.info(" ".join(repr(e).splitlines()))
-        logging.info(f"error while downloading image_id: {entry['image_id']}")
+        logging.error(f"image_id: {entry['image_id']}")
         logging.error(image_url)
+        logging.error(" ".join(repr(e).splitlines()))
+        logging.error("=" * 8)
         
-
+        
 if __name__ == '__main__':
 
+    split = 'valid'
+
+    image_root_dir = './cc3m_images/'
+    fn_prefix = f'cc3m_{split}'
+
     n_processes = 2
-    buf_size_per_proc = 500
+    buf_size_per_proc = 20
+    
     buf_size = buf_size_per_proc * n_processes
     buf = []
 
-    image_root_dir = './cc3m_images/'
-    fn_prefix = 'cc3m_train'
+
     target_dir = os.path.join(image_root_dir, fn_prefix)
+    
     if not os.path.isdir(target_dir):
         os.makedirs(target_dir, exist_ok=True)
-    if not os.path.isdir('./tmp/'):
-        os.makedirs('./tmp/', exist_ok=True)
 
-    cc3m_jsonl_file_path = './cc3m_valid.jsonl'
+    cc3m_jsonl_file_path = f'./cc3m_{split}.jsonl'
 
     with open(cc3m_jsonl_file_path, 'r', encoding='UTF-8') as fp:
 
@@ -90,7 +102,7 @@ if __name__ == '__main__':
 
             entry = json.loads(line)
 
-            if entry['id'] >= 1000:
+            if entry['id'] >= 40:
                 break
 
             buf.append((entry, target_dir, fn_prefix))
